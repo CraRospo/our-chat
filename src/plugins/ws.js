2258867b9ecb0a012b972f3e31a6c106981ee5e4
raochongwei <raochongwei@whty.com.cn>
2023-01-18 17:13:20 +0800

    chore: 修改ws 地址

--- src/plugins/ws.js
+++ src/plugins/ws.js
@@ -1,6 +1,9 @@
 export default {
   install: (app, options) => {
     const wsInstance = new WebSocket(options.url)
+    wsInstance.onerror = function (e) {
+      console.log(e)
+    }
     app.provide('ws', wsInstance)
   }
 }
